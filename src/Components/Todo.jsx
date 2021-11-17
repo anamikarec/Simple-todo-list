bd6415e7310c38a14e82affd580ea3ddf44ce822
Anamika <anamikarecsonbhadra@gmail.com>
2021-11-17 11:14:57 +0530

    react-todo-add done

---
+++ src/Components/Todo.jsx
@@ -0,0 +1,64 @@
+import {useState} from 'react';
+import Header from "./Header";
+import TodoInput from './TodoInput';
+import TodoItem from './TodoItem';
+
+const Todo = () =>{
+    const [todos, setTodos] = useState([
+        {
+            id: 1,
+            title: "Default",
+            status: "Incomplete"
+        }
+    ]);
+
+    const handleDelete = (id) =>{
+        setTodos(todos.filter((item) => item.id !== id))
+    };
+    const handleTaskCreate = (title)=>{
+        const payload = {
+            title: title,
+            status: 'Incomplete',
+            id: todos.length+1
+        };
+        setTodos([...todos, payload]);
+        // console.log(payload);
+    };
+
+
+    const handleToggle = (id) =>{
+        setTodos(todos.map((item) => item.id === id ? {...item,status: (item.status === "Done") ? ("Incomplete") : ("Done")} :item));
+    }
+    
+    // console.log(todos);
+    // Delete task
+    // const handleDelete = (id)=>{
+    //     setTodos(
+    //         todos.filter(item => item.id !==id)
+    //     )
+    // }
+
+    // togle task
+    // const handleToggle = (id) =>{
+    //     const updatedTodos = todos.map((item) => item.id===id? {...item,status: (item.status==='Done')?('Incomeplete'):'Done'}:item);
+    //     setTodos(updatedTodos);
+    // }
+
+    return (
+    <>
+        <Header />
+        <TodoInput onTaskCreate={handleTaskCreate} />
+        {todos.map((todos)=>(
+            < TodoItem
+              title={todos.title}
+              status={todos.status}
+              key={todos.id} 
+              id={todos.id} 
+           handleDelete = {handleDelete}
+            handleToggle = {handleToggle}
+            />
+        ))}
+    </>
+    );
+};
+export default Todo;
